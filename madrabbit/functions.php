451bd49dc7b80ff2cdd8a09bad33d7d02fdb73b0
nierdz <kevin@igln.fr>
2021-05-16 19:12:06 +0200

    refactor: remove sidebar and them settings code

--- madrabbit/functions.php
+++ madrabbit/functions.php
@@ -9,19 +9,15 @@
 defined( 'ABSPATH' ) || exit;
 
 $understrap_includes = array(
-  '/theme-settings.php',                  // Initialize theme default settings.
   '/setup.php',                           // Theme setup and custom theme supports.
-  '/widgets.php',                         // Register widget area.
   '/enqueue.php',                         // Enqueue scripts and styles.
   '/template-tags.php',                   // Custom template tags for this theme.
   '/pagination.php',                      // Custom pagination for this theme.
   '/extras.php',                          // Custom functions that act independently of the theme templates.
-  '/customizer.php',                      // Customizer additions.
   '/custom-comments.php',                 // Custom Comments file.
   '/jetpack.php',                         // Load Jetpack compatibility file.
   '/class-wp-bootstrap-navwalker.php',    // Load custom WordPress nav walker. Trying to get deeper navigation? Check out: https://github.com/understrap/understrap/issues/567.
   '/editor.php',                          // Load Editor functions.
-  '/deprecated.php',                      // Load deprecated functions.
 );
 
 foreach ( $understrap_includes as $file ) {
