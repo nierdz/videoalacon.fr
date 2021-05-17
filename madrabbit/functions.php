f24ea0ca0bffd897640b28c0f262dad6f5655221
nierdz <kevin@igln.fr>
2021-05-17 19:17:00 +0200

    refactor: remove editor customization shit

--- madrabbit/functions.php
+++ madrabbit/functions.php
@@ -17,7 +17,6 @@ $understrap_includes = array(
   '/custom-comments.php',                 // Custom Comments file.
   '/jetpack.php',                         // Load Jetpack compatibility file.
   '/class-wp-bootstrap-navwalker.php',    // Load custom WordPress nav walker. Trying to get deeper navigation? Check out: https://github.com/understrap/understrap/issues/567.
-  '/editor.php',                          // Load Editor functions.
 );
 
 foreach ( $understrap_includes as $file ) {
