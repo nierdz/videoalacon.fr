751b913db7a3d56333d9a6a6adb991445e50aab4
nierdz <kevin@igln.fr>
2021-05-18 06:21:11 +0200

    refactor: remove this shitty gulp
    - use make instead of gulp
    - remove boostrap and font-awesome from git (get them from npm)
    - remove generated files from git
    - lighter theme
    - closes #30

--- madrabbit/inc/enqueue.php
+++ madrabbit/inc/enqueue.php
@@ -22,8 +22,8 @@ if ( ! function_exists( 'understrap_scripts' ) ) {
 
     wp_enqueue_script( 'jquery' );
 
-    $js_version = $theme_version . '.' . filemtime( get_template_directory() . '/js/theme.min.js' );
-    wp_enqueue_script( 'understrap-scripts', get_template_directory_uri() . '/js/theme.min.js', array(), $js_version, true );
+    $js_version = $theme_version . '.' . filemtime( get_template_directory() . '/js/bootstrap.bundle.min.js' );
+    wp_enqueue_script( 'understrap-scripts', get_template_directory_uri() . '/js/bootstrap.bundle.min.js', array(), $js_version, true );
     if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
       wp_enqueue_script( 'comment-reply' );
     }
