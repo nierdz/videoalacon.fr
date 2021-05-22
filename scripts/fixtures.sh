4b253d112c2f96abf874ed7efb82f82635acefbd
nierdz <kevin@igln.fr>
2021-05-22 16:12:50 +0200

    feat: setup more efficient fixtures

--- scripts/fixtures.sh
+++ scripts/fixtures.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# To use this script you need to have an export name fixtures.xml
+# To create fixtures.xml file use this command:
+# wp export --dir=/tmp/ --post_type=post --start_id=19519 --post_type=post
+
 set -o errexit
 set -o pipefail
 set -o nounset
@@ -7,7 +11,11 @@ set -o nounset
 DEBUG=${DEBUG:=0}
 [[ $DEBUG -eq 1 ]] && set -o xtrace
 
-ROOT_URL="https://mad-rabbit.com"
+FIXTURES_FILE=${FIXTURES_FILE:-./fixtures.xml}
+
+if [[ ! -f "$FIXTURES_FILE" ]]; then
+  echo "You need to have fixtures.xml in root directory"
+fi
 
 # Create menu
 check_top_menu=$(docker exec \
@@ -54,46 +62,38 @@ for category in Animaux Étonnant Fail Marrant Rage Techno WTF; do
   fi
 done
 
-# Loop through posts from API v2
-posts_json=$(curl --silent --location --request GET "$ROOT_URL/wp-json/wp/v2/posts" | jq -c '.[]')
-while read -r post_json; do
-  date=$(echo "$post_json" | jq --raw-output '.date')
-  link=$(echo "$post_json" | jq --raw-output '.link')
-  content=$(echo "$post_json" | jq --raw-output '.content.rendered')
-  title=$(echo "$post_json" | jq --raw-output '.title.rendered')
-  category_json_url=$(echo "$post_json" | jq --raw-output '._links."wp:term"[0].href')
-  category_json=$(curl --silent --location --request GET "$category_json_url" | jq '.[]')
-  category=$(echo "$category_json" | jq --raw-output '.name')
-  tags_json_url=$(echo "$post_json" | jq --raw-output '._links."wp:term"[1].href')
-  tags_json=$(curl --silent --location --request GET "$tags_json_url" | jq -c '.[]')
-  tags_array=()
-  while read -r tag_json; do
-    tag_slug=$(echo "$tag_json" | jq --raw-output '.slug')
-    tags_array+=("$tag_slug")
-  done <<<"$tags_json"
-  html=$(curl -s "$link")
-  #video_url=$(echo "$html" | sed -nE '/og:video/s/.*content=\"(.*\.mp4)\" \/>/\1/p')
-  image_id=$(echo "$html" | sed -nE '/vthumbs/s/.*vthumbs\/([0-9]{10})\.jpg.*/\1/p')
-  tags=$(printf ",%s" "${tags_array[@]}")
-  tags=${tags:1}
-  post_id=$(docker exec \
-    -u www-data \
-    wordpress \
-    wp post create \
-    --post_date="$date" \
-    --tags_input="$tags" \
-    --post_author=1 \
-    --post_content="$content" \
-    --post_title="$title" \
-    --post_status="publish" \
-    --post_category="$category" \
-    --porcelain)
-  docker exec \
-    -u www-data \
-    wordpress \
+# Remove first post
+if docker exec -u www-data wordpress \
+  wp post exists 1; then
+  docker exec -u www-data wordpress \
+    wp post delete 1 --force
+fi
+
+# Install wordpress-importer and import posts from fixtures.xml
+docker exec -u www-data wordpress \
+  wp plugin install wordpress-importer --activate
+docker cp fixtures.xml wordpress:/tmp/
+docker exec -u www-data wordpress \
+  wp import /tmp/fixtures.xml --authors=skip
+
+# List all imported posts
+POST_LIST=$(sed -nE 's#  <link>(.*)</link>#\1#p' fixtures.xml | tail -n +1)
+
+# Loop through posts to import medias
+for post_url in ${POST_LIST}; do
+  html=$(curl -s "$post_url")
+  post_id=$(echo "$html" | sed -nE 's#.*wp-json/wp/v2/posts/([0-9]*)".*#\1#p')
+  post_title=$(echo "$html" | sed -nE 's#.*>(.*)</h1>.*#\1#p')
+  image_id=$(echo "$html" | sed -nE 's#.*vthumbs/([0-9]{10})\.jpg.*#\1#p')
+  docker exec -u www-data wordpress \
     wp media import \
     "https://media.mad-rabbit.com/vthumbs/$image_id.jpg" \
     --post_id="$post_id" \
-    --title="$title" \
+    --title="$post_title" \
     --featured_image
-done <<<"$posts_json"
+  docker exec -u www-data wordpress \
+    wp media import \
+    "https://media.mad-rabbit.com/videos/$image_id.mp4" \
+    --post_id="$post_id" \
+    --title="$post_title"
+done
