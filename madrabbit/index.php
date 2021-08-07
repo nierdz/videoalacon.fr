b08bc92d9ca78bdbfd58192dc5f2a59c7ddf0ad0
nierdz <kevin@igln.fr>
2021-08-07 07:26:31 +0200

    feat: enhanced homepage

--- madrabbit/index.php
+++ madrabbit/index.php
@@ -19,31 +19,31 @@ get_header();
 
       <main class="site-main" id="main">
 
+        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-4">
+
         <?php
         if ( have_posts() ) {
           while ( have_posts() ) {
             the_post();
         ?>
 
-        <div class="card mb-3">
-          <div class="row no-gutters">
-            <div class="col-md-4">
-              <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('large'); ?></a>
-            </div>
-            <div class="col-md-8">
-              <div class="card-body">
-                <?php the_title( sprintf( '<h2 class="card-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
-                <p class="card-text"><?php the_excerpt(); ?></p>
-                <p class="card-text"><small class="text-muted">Publié le <?php echo get_the_date('F j, Y') ?></small></p>
+          <div class="col">
+            <div class="card mb-3">
+              <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail( 'large', array('class' => 'index-image-cards')); ?></a>
+              <div class="card-body text-truncate">
+                <?php the_title( sprintf( '<h6 class="card-title"><a href="%s" class="text-decoration-none" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h6>' ); ?>
+                <div class="card-text"><i class="bi-calendar"></i> <small class="text-muted"><?php echo get_the_date('j F Y') ?></small></div>
+                <div class="card-text"><i class="bi-clock"></i> <small class="text-muted"><?php echo esc_html( get_post_meta( get_the_ID(), 'video_duration', true ) ) ?></small></div>
               </div>
             </div>
           </div>
-        </div>
 
         <?php
           }
         }
         ?>
+
+        </div>
 
       </main><!-- #main -->
 
